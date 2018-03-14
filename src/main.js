48b46073ad51182ca81f71fb7a6f20f0456a79ef
Elity <xing.suifeng@gmail.com>
2018-03-14 19:31:57 +0800

    request改造，修改mock适配新的request

--- src/main.js
+++ src/main.js
@@ -1,43 +1,44 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+import "normalize.css/normalize.css"; // A modern alternative to CSS resets
 
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import Element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
-import '@/styles/index.scss' // global css
+import "@/styles/index.scss"; // global css
 
-import App from './App'
-import router from './router'
-import store from './store'
-import Cookies from 'js-cookie'
-Cookies.set('language', 'zh')
+import App from "./App";
+import router from "./router";
+import store from "./store";
+import Cookies from "js-cookie";
+Cookies.set("language", "zh");
 
-import i18n from './lang' // Internationalization
-import './icons' // icon
-import './errorLog'// error log
-import './permission' // permission control
-import './mock' // simulation data
+import i18n from "./lang"; // Internationalization
+import "./icons"; // icon
+import "./errorLog"; // error log
+import "./asyncErrorHandle"; // async error handle
+import "./permission"; // permission control
+import "./mock"; // simulation data
 
-import * as filters from './filters' // global filters
+import * as filters from "./filters"; // global filters
 
 Vue.use(Element, {
-  size: 'medium', // set element-ui default size
+  size: "medium", // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
-})
+});
 
 // register global utility filters.
 Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key])
-})
+  Vue.filter(key, filters[key]);
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
   i18n,
-  template: '<App/>',
+  template: "<App/>",
   components: { App }
-})
+});
--- src/mock/index.js
+++ src/mock/index.js
@@ -1,29 +1,45 @@
-import Mock from 'mockjs'
-import loginAPI from './login'
-import articleAPI from './article'
-import remoteSearchAPI from './remoteSearch'
-import transactionAPI from './transaction'
+import Mock from "mockjs";
+import loginAPI from "./login";
+import articleAPI from "./article";
+import remoteSearchAPI from "./remoteSearch";
+import transactionAPI from "./transaction";
+import qs from "qs";
+
+function mock(url, type, fn) {
+  function wrap(fn) {
+    return function(config) {
+      config.body = JSON.stringify(qs.parse(config.body));
+      return { status: 0, data: fn(config) };
+    };
+  }
+  if (typeof fn === "function") {
+    fn = wrap(fn);
+  } else if (typeof type === "function") {
+    type = wrap(type);
+  }
+  Mock.mock(url, type, fn);
+}
 
 // Mock.setup({
 //   timeout: '350-600'
 // })
 
 // 登录相关
-Mock.mock(/\/login\/login/, 'post', loginAPI.loginByUsername)
-Mock.mock(/\/login\/logout/, 'post', loginAPI.logout)
-Mock.mock(/\/user\/info\.*/, 'get', loginAPI.getUserInfo)
+mock(/\/login\/login/, "post", loginAPI.loginByUsername);
+mock(/\/login\/logout/, "post", loginAPI.logout);
+mock(/\/user\/info\.*/, "get", loginAPI.getUserInfo);
 
 // 文章相关
-Mock.mock(/\/article\/list/, 'get', articleAPI.getList)
-Mock.mock(/\/article\/detail/, 'get', articleAPI.getArticle)
-Mock.mock(/\/article\/pv/, 'get', articleAPI.getPv)
-Mock.mock(/\/article\/create/, 'post', articleAPI.createArticle)
-Mock.mock(/\/article\/update/, 'post', articleAPI.updateArticle)
+mock(/\/article\/list/, "get", articleAPI.getList);
+mock(/\/article\/detail/, "get", articleAPI.getArticle);
+mock(/\/article\/pv/, "get", articleAPI.getPv);
+mock(/\/article\/create/, "post", articleAPI.createArticle);
+mock(/\/article\/update/, "post", articleAPI.updateArticle);
 
 // 搜索相关
-Mock.mock(/\/search\/user/, 'get', remoteSearchAPI.searchUser)
+mock(/\/search\/user/, "get", remoteSearchAPI.searchUser);
 
 // 账单相关
-Mock.mock(/\/transaction\/list/, 'get', transactionAPI.getList)
+mock(/\/transaction\/list/, "get", transactionAPI.getList);
 
-export default Mock
+export default Mock;
