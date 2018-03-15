4b0daafc72927720220c7b2c236636bfa4d064d4
Emily <342019172@qq.com>
2018-03-15 15:29:19 +0800

    类目管理api

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,10 @@
 export default {
     login: '/login/login',
-    logout: '/login/logout'
+    logout: '/login/logout',
+    getListCategory: '/category/listCategory',
+    addCategory: '/category/addCategory',
+    reviseCategory: '/category/editCategory',
+    delCategory: '/category/delCategory',
+    getCategoryCourse: '/category/listCategoryCourse',
+    categoryCourseSort: '/banner/setCategoryCourseSort'
 }
--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -8,7 +8,7 @@
           <span>{{index+1}}</span>
         </template>
             <template slot="handle" slot-scope="{row,index}">
-                <el-button type='text' v-if='index==0' @click="dialogTableVisible1 =true">编辑课程</el-button>
+                <el-button @click="jump('getCategoryList')" type='text' v-if='index==0'>编辑课程</el-button>
                 <el-button type='text' v-else @click="editCategory(index,row.categoryName)">编辑类目名称</el-button>
                 &nbsp;&nbsp;&nbsp;
                 <el-button  type='text' @click="del(index)" v-if='index!=0'>删除</el-button>
@@ -26,30 +26,9 @@
             </div>
         </el-dialog>
 
-<!-- 编辑课程弹窗 -->
-        <el-dialog title="编辑课程" :visible.sync="dialogTableVisible1" width='35%' ref='dataForm'>
-            <div class='marginBottom'>
-              类目名称：<el-input v-model="input1" placeholder="课程名或课程id进行查询" clearable  size="small"></el-input>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-            <el-button type="primary">查询</el-button>&nbsp;&nbsp;
-            <el-button>重置</el-button>
-            </div>
-            <TablePager :data='courses' :columns="columns2"  :pagination="pagination">
-              <template slot='position' slot-scope='{row,index}'>
-                <span>{{index+1}}</span>
-              </template>
-              <template slot="sort" slot-scope="{row}">
-                <span>
-                  <el-button  v-if="row.sort == 'start'" type="text">下移 删除</el-button> 
-                  <el-button  v-else-if="row.sort == 'end'" type="text">上移 删除</el-button> 
-                  <el-button v-else type="text">上移 下移 删除</el-button> 
-                  </span>
-              </template>
-            </TablePager>
-        </el-dialog>
 
-<!-- 编辑类目名称弹窗 -->
-        <el-dialog title="修改类目名称" :visible.sync="dialogTableVisible2" width='35%' ref='dataForm'>
+<!-- 修改类目名称弹窗 -->
+        <el-dialog title="编辑类目名称" :visible.sync="dialogTableVisible2" width='35%' ref='dataForm'>
             <div class=''>
               类目名称：<el-input v-model="name" placeholder="" clearable  size="small"></el-input>
 <br><br>
@@ -61,22 +40,22 @@
 </template>
 
 <script>
-import { fetchList } from "@/api/article";
+import { listCategory, categoryAdd } from "@/api/category";
 import TablePager from "@/components/TablePager";
 export default {
   name: "category",
   data() {
     return {
       listLoading: true,
+      list: [],
       name: "",
       reviseName: "",
       dialogTableVisible: false,
-      dialogTableVisible1: false,
       dialogTableVisible2: false,
       temp: {
         categoryId: "",
         categoryName: "",
-        handle: "编辑课程",
+        handle: "",
         del: "删除"
       },
       input1: "",
@@ -90,49 +69,40 @@ export default {
         { title: "类目名称", key: "categoryName" },
         { title: "操作管理", slot: "handle" }
       ],
-      data: [
-        { categoryName: "推荐课程", handle: "0" },
-        { categoryName: "交互设计", handle: "1" }
-      ],
-      columns2: [
-        { title: "位置", slot: "position" },
-        { title: "课程id", key: "courseId" },
-        { title: "课程名称", key: "courseName" },
-        { title: "排序", slot: "sort" }
-      ],
-      courses: [
-        { courseId: "25", courseName: "设计质量", sort: "start" },
-        { courseId: "25", courseName: "设计质量", sort: "" },
-        { courseId: "13", courseName: "工业设计", sort: "end" }
-      ]
+      data: [{ categoryName: "推荐课程" }, { categoryName: "交互设计" }]
     };
   },
   components: {
     TablePager
   },
   filters: {},
-  created() {},
+  created() {
+    // this.getList();
+  },
   methods: {
+    //  页面跳转
+    jump(pageType, courseId) {
+      this.$router.push({ name: pageType, params: {} });
+    },
     //获取类目列表
     getList() {
-      getListCategory().then(res => {});
+      listCategory().then(res => {
+        return (this.data = res.data.category);
+      });
     },
     resetTemp() {
       this.temp = {
         categoryId: "",
-        categoryName: "",
-        handle: "编辑课程",
-        del: "删除"
+        categoryName: ""
       };
     },
-    handleCreate() {
-      this.resetTemp();
-      (this.dialogStatus = "create"), (this.dialogTableVisible = true);
-      // this.$refs['dataForm'].
-    },
+    //新增类目
     updateData() {
-      this.data.push(this.temp);
-      this.dialogTableVisible = false;
+      // categoryAdd(this.temp.categoryName).then(res => {
+        this.data.push(this.temp);
+        this.dialogTableVisible = false;
+        this.resetTemp();
+      // });  
     },
     del(index) {
       this.data.splice(index, 1);
@@ -143,6 +113,7 @@ export default {
         duration: 2000
       });
     },
+    //编辑类目
     editCategory(index, name) {
       this.dialogTableVisible2 = true;
       this.name = name;
