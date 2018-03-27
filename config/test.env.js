ff0c4578ab1c784f40c3b4f9932b51663c1b36e5
yanping <752789782@qq.com>
2018-03-27 17:58:02 +0800

    'test环境请求地址修改'

--- config/test.env.js
+++ config/test.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"test"',
-  BASE_API: '"https://micro-test.shiguangkey.com"'
+  BASE_API: '"https://micro-admin-test.shiguangkey.com"'
 };
