d139e080370d940de9297d683ba0851019514a51
Emily <342019172@qq.com>
2018-04-09 17:01:45 +0800

    '更改字段'

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -140,7 +140,7 @@ export default {
     //删除类目
     async del(index, categoryId) {
       try {
-        await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        await this.$confirm('此操作将永久删除该类目, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
