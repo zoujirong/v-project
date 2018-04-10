f7dc95c98cc1d8a0baec3c8655a20535392a325e
amy <1334186686@qq.com>
2018-04-10 19:47:08 +0800

    注释取消

--- src/views/course/detail.vue
+++ src/views/course/detail.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="app-container">
-    123456
-    <!-- <el-form ref="editForm" label-suffix="：" label-width="150px" :model="course" :rules="rules" :inline-message="true">
+    <el-form ref="editForm" label-suffix="：" label-width="150px" :model="course" :rules="rules" :inline-message="true">
       <el-form-item label="课程标题" prop="title">
         <el-input v-model.trim="course.title" class="short-input" :maxlength="40"></el-input>
       </el-form-item>
@@ -30,11 +29,11 @@
       </el-form-item>
       <el-form-item label="课程封面" prop="courseCover" :rules="[{required: true,message: '请上传课程封面！'}]">
         <upload-image :limit="1" :fileList="course.courseCover ? [{url: course.courseCover}] : []" @onSuccess="onUploadCover"></upload-image>
-        <span class="form-tips">要求：图片宽高像素分别为 X * Y</span>
+        <!-- <span class="form-tips">要求：图片宽高像素分别为 X * Y</span> -->
       </el-form-item>
       <el-form-item label="课程介绍" prop="courseDesc" :rules="[{required: true,message: '请上传课程介绍！'}]">
         <upload-image :limit="1" :fileList="course.courseDesc ? [{url: course.courseDesc}] : []" @onSuccess="onUploadDetail"></upload-image>
-        <span class="form-tips">要求：建议图片宽度为**像素，高度不超过**像素</span>
+        <!-- <span class="form-tips">要求：建议图片宽度为**像素，高度不超过**像素</span> -->
       </el-form-item>
       <el-form-item label="课程价格" prop="coursePrice">
         <el-input-number controls-position="right" :min="0.00" :max="999999" :step="0.01" v-model="course.coursePrice"></el-input-number>
@@ -47,130 +46,130 @@
         <el-button type="primary" @click="submit" :loading="loading">保存</el-button>
         <router-link :to="{name: 'courseList'}" tag="el-button">返回</router-link>
       </el-form-item>
-    </el-form> -->
+    </el-form>
   </div>
 </template>
 
 <script>
-// import UploadImage from '@/components/UploadImage';
-// import { getCourseDetail, updateCourse, addCourse } from '@/api/course';
-// import { listCategory } from '@/api/category';
-// import { teacherList } from '@/api/teacher';
+import UploadImage from '@/components/UploadImage';
+import { getCourseDetail, updateCourse, addCourse } from '@/api/course';
+import { listCategory } from '@/api/category';
+import { teacherList } from '@/api/teacher';
 
-// export default {
-//   data() {
-//     let self = this;
-//     return {
-//       commonParam: {
-//         pageNo: 1,
-//         pageSize: 100
-//       },
-//       teacherList: [],
-//       categoryList: [],
-//       loading: false,
-//       courseId: this.$route.query.id,
-//       course: {
-//         title: '',
-//         teachingMethod: 0, //0直播,1录播
-//         categoryId: '',
-//         tzCourseId: '',
-//         mainTeacher: '',
-//         courseCover: '',
-//         courseDesc: '',
-//         coursePrice: 0.0,
-//         customerWx: ''
-//       },
-//       rules: {
-//         title: [
-//           { required: true, message: '请填写课程标题！' },
-//           {
-//             validator: (field, value, callback) => {
-//               let msg;
-//               if (value && +value == value) msg = '课程标题不能是纯数字';
-//               callback(msg);
-//             }
-//           }
-//         ],
-//         tzCourseId: [
-//           {
-//             validator: (field, value, callback) => {
-//               let msg;
-//               if (self.course.teachingMethod === 0 && !value)
-//                 msg = '潭州课程ID不能为空';
-//               else if (value && !/^\d+$/.test(value))
-//                 msg = '请填写正确的潭州课程ID';
-//               callback(msg);
-//             }
-//           }
-//         ]
-//       }
-//     };
-//   },
-//   components: { UploadImage },
-//   methods: {
-//     async getCourseDetailById() {
-//       let res = await getCourseDetail(this.courseId);
-//       this.course = res.data;
-//     },
-//     async getTeacher() {
-//       let res = await teacherList(this.commonParam);
-//       this.teacherList = res.data.data;
-//     },
-//     async getCategory() {
-//       let res = await listCategory(this.commonParam);
-//       this.categoryList = res.data.data;
-//     },
-//     onUploadCover(urls) {
-//       console.log('上传封面', urls);
-//       this.course.courseCover = urls[0] || '';
-//     },
-//     onUploadDetail(urls) {
-//       this.course.courseDesc = urls[0] || '';
-//       console.log('上传详情', urls);
-//     },
-//     success(response, file, fileList) {
-//       console.log(response, file, fileList);
-//     },
-//     changeMethod(a) {
-//       this.$refs.editForm.validateField('tzCourseId');
-//     },
-//     async submit() {
-//       let form = this.$refs.editForm;
-//       let res = await form
-//         .validate()
-//         .then(res => {
-//           if (!res) return Promise.reject({ msg: '信息填写有误' });
-//           this.loading = true;
-//           if (this.courseId) {
-//             return updateCourse({
-//               id: this.courseId,
-//               ...this.course
-//             });
-//           }
-//           return addCourse(this.course);
-//         })
-//         .finally(res => {
-//           this.loading = false;
-//         });
-//       //修改课程
-//       if (this.courseId) {
-//         this.$message.success('修改课程成功');
-//         this.$router.push({ name: 'courseList' });
-//       } else {
-//         this.$router.push({
-//           name: 'courseChapter',
-//           params: { id: res.data.courseId },
-//           query: { type: this.course.teachingMethod }
-//         });
-//       }
-//     }
-//   },
-//   created() {
-//     this.courseId && this.getCourseDetailById();
-//     this.getCategory();
-//     this.getTeacher();
-//   }
-// };
+export default {
+  data() {
+    let self = this;
+    return {
+      commonParam: {
+        pageNo: 1,
+        pageSize: 100
+      },
+      teacherList: [],
+      categoryList: [],
+      loading: false,
+      courseId: this.$route.query.id,
+      course: {
+        title: '',
+        teachingMethod: 0, //0直播,1录播
+        categoryId: '',
+        tzCourseId: '',
+        mainTeacher: '',
+        courseCover: '',
+        courseDesc: '',
+        coursePrice: 0.0,
+        customerWx: ''
+      },
+      rules: {
+        title: [
+          { required: true, message: '请填写课程标题！' },
+          {
+            validator: (field, value, callback) => {
+              let msg;
+              if (value && +value == value) msg = '课程标题不能是纯数字';
+              callback(msg);
+            }
+          }
+        ],
+        tzCourseId: [
+          {
+            validator: (field, value, callback) => {
+              let msg;
+              if (self.course.teachingMethod === 0 && !value)
+                msg = '潭州课程ID不能为空';
+              else if (value && !/^\d+$/.test(value))
+                msg = '请填写正确的潭州课程ID';
+              callback(msg);
+            }
+          }
+        ]
+      }
+    };
+  },
+  components: { UploadImage },
+  methods: {
+    async getCourseDetailById() {
+      let res = await getCourseDetail(this.courseId);
+      this.course = res.data;
+    },
+    async getTeacher() {
+      let res = await teacherList(this.commonParam);
+      this.teacherList = res.data.data;
+    },
+    async getCategory() {
+      let res = await listCategory(this.commonParam);
+      this.categoryList = res.data.data;
+    },
+    onUploadCover(urls) {
+      console.log('上传封面', urls);
+      this.course.courseCover = urls[0] || '';
+    },
+    onUploadDetail(urls) {
+      this.course.courseDesc = urls[0] || '';
+      console.log('上传详情', urls);
+    },
+    success(response, file, fileList) {
+      console.log(response, file, fileList);
+    },
+    changeMethod(a) {
+      this.$refs.editForm.validateField('tzCourseId');
+    },
+    async submit() {
+      let form = this.$refs.editForm;
+      let res = await form
+        .validate()
+        .then(res => {
+          if (!res) return Promise.reject({ msg: '信息填写有误' });
+          this.loading = true;
+          if (this.courseId) {
+            return updateCourse({
+              id: this.courseId,
+              ...this.course
+            });
+          }
+          return addCourse(this.course);
+        })
+        .finally(res => {
+          this.loading = false;
+        });
+      //修改课程
+      if (this.courseId) {
+        this.$message.success('修改课程成功');
+        this.$router.push({ name: 'courseList' });
+      } else {
+        this.$router.push({
+          name: 'courseChapter',
+          params: { id: res.data.courseId },
+          query: { type: this.course.teachingMethod }
+        });
+      }
+    }
+  },
+  created() {
+    this.courseId && this.getCourseDetailById();
+    this.getCategory();
+    this.getTeacher();
+  }
+};
 </script>
 <style scoped>
 .el-input,
